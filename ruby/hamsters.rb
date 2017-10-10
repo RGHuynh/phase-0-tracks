a461a307bf3dfc436f1ca6d344a719438b3035c7
RGHuynh <randall.g.huynh@gmail.com>
2017-10-10 09:13:49 -0400

    Fix user input types
    The answer type the user input can vary depending on the questions
    asked. Using the converting type method at the end of the input method
    will make sure the type is correct.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,8 +3,8 @@ print "What's the hamster name?: "
 hamster_name = gets.chomp
 
 # Volumn level of the hamster
-print "What's the hmaster volumn level would you like?: "
-hamster_volumn_lvl = gets.chomp
+print "What's the hmaster volumn level would you like?(1 to 10): "
+hamster_volumn_lvl = gets.chomp.to_f
 
 # fur color
 print "What's the fur color do you want?: "
@@ -16,4 +16,4 @@ user_adoption_input = gets.chomp
 
 # estimate age
 print "What is the estimate age of the hamster?: "
-hamster_est_age = gets.chomp
+hamster_est_age = gets.chomp.to_i
