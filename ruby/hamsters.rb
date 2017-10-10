bb507bda57a877829def8899ffad90f63c347355
RGHuynh <randall.g.huynh@gmail.com>
2017-10-10 09:00:01 -0400

    Add user questions/input for hamster adoption
    Used print method to ask user hamster questions for adoption. The
    reason print was used is because it doesn't skip to the next line
    when the method is execute. "gets.chomp" was used to get value input
    from users.

---
+++ ruby/hamsters.rb
@@ -0,0 +1,19 @@
+# Get hamster name
+print "What's the hamster name?: "
+hamster_name = gets.chomp
+
+# Volumn level of the hamster
+print "What's the hmaster volumn level would you like?: "
+hamster_volumn_lvl = gets.chomp
+
+# fur color
+print "What's the fur color do you want?: "
+hamster_fur_clr = gets.chomp
+
+# Whether the hamster is a good candidate for adoption
+print "Do you think the hamster is good for adoption?: "
+user_adoption_input = gets.chomp
+
+# estimate age
+print "What is the estimate age of the hamster?: "
+hamster_est_age = gets.chomp
