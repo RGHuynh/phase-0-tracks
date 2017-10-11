1d9746519aa644563494b516658487afb0261515
RGHuynh <randall.g.huynh@gmail.com>
2017-10-11 14:39:44 -0400

    Add case statement to interview questions
    The case statement was used to help run through each scenario starting
    with the case that has the lowest chance of being true. Then it will
    print the answer onto the screen after the condition are met.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,19 +1,38 @@
-# Ask for user name
-print "What is your name?: "
-user_name = gets.chomp
 
-# Ask user how old are they
-print "How old are you?: "
-user_age = gets.chomp
+  # Ask for user name
+  print "What is your name?: "
+  user_name = gets.chomp
 
-# Ask user what year were they born
-print "What year were you born?: "
-user_year_born = gets.chomp
+  # Ask user how old are they
+  print "How old are you?: "
+  user_age = gets.chomp
 
-# Ask user about galic bread
-print "Our company cafeteria serves garlic bread. Should we order some for you?:"
-order_answer = gets.chomp
+  # Ask user what year were they born
+  print "What year were you born?: "
+  user_year_born = gets.chomp
 
-# Ask user about insurance plan
-print "Would you like to enroll in the company health insurance?: "
-user_enrollment_ans = gets.chomp
+  # test age t/f
+  current_year = Time.now.year
+  user_est_age = current_year - user_year_born.to_i
+
+  # Ask user about galic bread
+  print "Our company cafeteria serves garlic bread. Should we order some for you?(y/n):"
+  order_answer = gets.chomp
+
+  # Ask user about insurance plan
+  print "Would you like to enroll in the company health insurance?(y/n): "
+  user_enrollment_ans = gets.chomp
+
+
+case
+when user_est_age.to_s != user_age && order_answer == "n" && user_enrollment_ans == "n" && user_name == "Drake Cula" || user_name == "Tu Fang"
+  puts "Definitely a vampire."
+when user_est_age.to_s == user_age && order_answer == "y"
+  puts "Probably not a vampire."
+when (user_est_age.to_s != user_age && order_answer == "n") && user_enrollment_ans == "n"
+  puts "Almost certainly a vampire."
+when user_est_age.to_s != user_age && (order_answer == "n" || user_enrollment_ans == "n")
+  puts "Probably a vampire."
+else
+  puts "Results inconclusive."
+end
