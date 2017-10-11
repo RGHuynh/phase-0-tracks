890f6adb62c920492f2734806b86d7b9137e9155
RGHuynh <randall.g.huynh@gmail.com>
2017-10-11 11:07:01 -0400

    Add questions for interviewing vamp
    Using print over puts to display the questions was a better user
    experience because it helps display the input answer right next to the
    question.

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+# Ask for user name
+print "What is your name?: "
+user_name = gets.chomp
+
+# Ask user how old are they
+print "How old are you?: "
+user_age = gets.chomp
+
+# Ask user what year were they born
+print "What year were you born?: "
+user_year_born = gets.chomp
+
+# Ask user about galic bread
+print "Our company cafeteria serves garlic bread. Should we order some for you?:"
+order_answer = gets.chomp
+
+# Ask user about insurance plan
+print "Would you like to enroll in the company health insurance?: "
+user_enrollment_ans = gets.chomp
