04ee1dda5e2891a8532d9aab9424b33e6db44d8e
RGHuynh <randall.g.huynh@gmail.com>
2017-10-12 14:35:10 -0400

    Add pseudocode for encrypt method

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,5 @@
+# An encrypt method that advances every letter of a string one letter forward
+# make an empty string variable
+# decontruct each letter in the variable
+# move each letter one letter forward
+# recontruct it
