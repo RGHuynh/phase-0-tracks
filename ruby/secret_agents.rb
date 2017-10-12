d51204a83318ed6a8203f6518c154092b05b850a
RGHuynh <randall.g.huynh@gmail.com>
2017-10-12 15:13:27 -0400

    Add encryption method to feature
    The program will ask the user for their pw. Then it will pass the pw
    into the encrypt_password method. The method will deconstruct the pw.
    Using the for loop base on the length of the pw, it will identify
    each letter based on the pw index, move a letter forward, then store it
    to an empty variable.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,5 +1,18 @@
 # An encrypt method that advances every letter of a string one letter forward
-# make an empty string variable
-# decontruct each letter in the variable
-# move each letter one letter forward
+# ask user for their password
+puts "Make a password."
+
+# make an input variable and find lenght of string -1
+user_password = gets.chomp
+pw_length = user_password.length - 1
+
+# space to put the encrypt pw
 # recontruct it
+encypt_pw = ""
+
+# decontruct each letter in the variable
+for pw_breakdown in 0..pw_length
+  indv_pw_letter = user_password[pw_breakdown]
+  # move each letter one letter forward
+  encypt_pw += indv_pw_letter.next
+end
