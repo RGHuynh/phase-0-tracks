c5bb49df84ad47030a5c4307f94eb29ed2e31064
RGHuynh <randall.g.huynh@gmail.com>
2017-10-12 16:37:46 -0400

    Add a decoder method
    The decoder will take the encrypted message, measure the length of
    the pw, reverse the index order starting at -1 to scan the last letter
    of the pw. Afterward, match the index numbers to the alphabet
    string to identify the letter preceding the original letter. Then
    stored each letter into a variable call decoded_password with the
    variable string reverse to display the correct value.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,18 +1,46 @@
 # An encrypt method that advances every letter of a string one letter forward
-# ask user for their password
-puts "Make a password."
+def encrypt_password(pw)
 
-# make an input variable and find lenght of string -1
-user_password = gets.chomp
-pw_length = user_password.length - 1
+  # make an input variable and find lenght of string -1
+  user_password = pw
+  pw_length = user_password.length - 1
 
-# space to put the encrypt pw
-# recontruct it
-encypt_pw = ""
+  # space to put the encrypt pw
+  # recontruct it
+  encrypt_pw = ""
 
-# decontruct each letter in the variable
-for pw_breakdown in 0..pw_length
-  indv_pw_letter = user_password[pw_breakdown]
-  # move each letter one letter forward
-  encypt_pw += indv_pw_letter.next
+  # decontruct each letter in the variable
+  for pw_breakdown in 0..pw_length
+    indv_pw_letter = user_password[pw_breakdown]
+    # move each letter one letter forward
+    encrypt_pw += indv_pw_letter.next
+  end
+  encrypt_pw
 end
+
+# reenter encrypted code
+def decode_pw(encrypted_pw)
+  # set alphabet for reference
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  to_decode = encrypted_pw
+  decoded_password = ""
+  # deconstruct variable
+  pw_length = to_decode.length
+  for encrypted_pw_breakdown in 1..pw_length
+    negative_pw_index = -(encrypted_pw_breakdown)
+    pw_indv_num = to_decode[negative_pw_index]
+    # match each word to the a..z string
+    # minus 1 per index number found
+    alpha_search = alphabet.index(pw_indv_num) - 1
+    # find the letter in a..z string
+    decoded_password += alphabet[alpha_search]
+  end
+  # recontruct
+  rev_pw = decoded_password.reverse
+end
+
+# ask user for their password
+puts "Enter you password!"
+user_pw_input = gets.chomp
+
+secure_pw = encrypt_password(user_pw_input)
