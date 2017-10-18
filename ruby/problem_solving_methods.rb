4103b94bea23ef6a8f4f7aa135947d74e71da386
RGHuynh <randall.g.huynh@gmail.com>
2017-10-18 14:22:21 -0400

    Add a fib method
    The fib method takes the argument and make fibonacci numbers by running
    it through a loop. The loop will store it into an array.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,8 +1,10 @@
+# Release 0
 # testing array
+
 array_test = [1,2,3,4,5]
 
 # method to pass the array_test and search index to
-def search_array(array, index)
+def search_array(array, search_int)
   # have an empty array to store the index array numbers
   index_array = []
 
@@ -10,7 +12,7 @@ def search_array(array, index)
 
   # convert array and index to string
   array_string = array.map {|x| x.to_s}
-  index_string = index.to_s
+  index_string = search_int.to_s
 
   # make a counter for 0 to array lenght numbers
   for x in 0..length_of_array
@@ -32,3 +34,25 @@ def search_array(array, index)
 end
 
 search_array(array_test, 1)
+
+# Release 1
+def fib(input_number)
+  old_number = 0
+  new_number = 1
+
+  # passing info to the array
+  fib_storage = [0,1]
+
+  y = input_number - 2
+
+  for x in 1..y
+    next_number = new_number + old_number
+    fib_storage << next_number
+    old_number = new_number
+    new_number = next_number
+
+  end
+  p fib_storage
+end
+
+fib(100)
