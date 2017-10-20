228b783673be9eb55d43108c979569920d4b91d9
RGHuynh <randall.g.huynh@gmail.com>
2017-10-20 09:58:44 -0400

    Test how to build instance method in class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,44 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  def bark(count)
+    count.times do |i|
+      puts "Woof!"
+    end
+  end
 
+  def rollover
+    puts "rollover"
+  end
+
+  def dog_years(human_years)
+    # take human_years and times it by dog year
+    dog_yr = human_years * 5
+    puts dog_yr
+  end
+
+  def jump(times)
+    for n in 1..times
+      if n < times
+        puts "Yay! I'm doing a back flip"
+      else
+        puts "Gog i'm tired"
+      end
+    end
+  end
+
+  def initializ
+    puts "Initializing new puppy instance ..."
+  end
 end
 
+bobby = Puppy.new
+
+# bobby.fetch("tree")
+bobby.bark(3)
+bobby.rollover
+
+bobby.dog_years(4)
+
+bobby.jump(4)
+bobby.initializ
