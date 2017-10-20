35f348ce7329d4b179c29d42935cae52bbc6fb5a
RGHuynh <randall.g.huynh@gmail.com>
2017-10-20 09:18:27 -0400

    Added a pseudo code for a dog class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Species ----
+# doggy b
+
+# CHARACTERISTICS -------
+# Eyelash length: 8
+# Friendly: yes
+# Eye count: 2
+# Name: Jammie boy
+# Color: red
+#
+# BEHAVIOR -------
+# fly
+# runn fast
+# crazy
