24f75d7574b6a13fef3487e7edd6fcb302091ae0
RGHuynh <randall.g.huynh@gmail.com>
2017-10-25 16:14:00 -0400

    Add an array of Santa classes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,12 @@
 class Santa
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance...."
+    @user_gender = gender
+    @user_ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph","Dasher","Dancer","Prancer","Vixen","Comet","Cupid","Donner", "Blizten"]
+    @age = 0
+  end
+
   def speak
     puts "Ho,ho, ho! Haaaaapy holiday"
   end
@@ -7,16 +15,22 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
-  def initialize
-    puts "Initializing Santa instance...."
-    gender
-    ethnicity
-  end
-
-  def shit
-    puts "santa needs to take a shit"
-  end
 end
 
-p x = Santa.new
-p x.speak
+
+# empty container for the Santa collection
+santas = []
+gender = ["agender","female","bigender","male", "female","gender fluid", "N/A"]
+ethnicity = ["black","Latino","white","Japanese","prefer not to say","Mystical Creature(unicorn)","N/A"]
+
+gender.length.times do |time|
+  santas << Santa.new(gender[time],ethnicity[time])
+end
+
+gender2 = ["cat","dog","fat"]
+ethnicity2 = ["crazy","wild","cookie"]
+
+# combine each of gender2 with ethnicity2 one by one
+gender2.length.times do |t|
+  santas << Santa.new(gender2[t], ethnicity2[t])
+end
