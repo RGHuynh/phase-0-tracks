f912ee188a130b5713cdecaa16391b02edb73576
RGHuynh <randall.g.huynh@gmail.com>
2017-10-26 17:37:34 -0400

    Add self methods to Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-  #methods goes here
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily
+    puts "crazy!!"
+  end
 end
+
+p Shout.yell_angrily("hey")
+p Shout.yelling_happily
