1c3b9ef5be7947dee50058e5b3687f42fca39790
RGHuynh <randall.g.huynh@gmail.com>
2017-10-26 14:05:21 -0400

    Add random santa generator
    The program will ask the user for how many santa they want to
    generate. It will take the user input and then loop it through
    that amount of time. Each time generating a random santa gender
    and ethnicity that will be put into random_santas data base. The
    program will end with printing the attributes of the generated santa
    age, gender, ethnicity.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,12 +1,13 @@
 class Santa
   attr_reader :user_gender
   attr_accessor :age, :user_ethnicity
+
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance...."
     @user_gender = gender
     @user_ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph","Dasher","Dancer","Prancer","Vixen","Comet","Cupid","Donner", "Blizten"]
-    @age = 0
+    @age = rand(140)
   end
 
   def speak
@@ -31,30 +32,43 @@ class Santa
     @reindeer_ranking << reindeer_name
 
     # delete the reindeer name
-
     @reindeer_ranking.delete_at(reindeer_index)
   end
-
-
 end
 
+# ask user how many santa do they want
+puts "How many santa slaves you want to create?"
+slave_amount = gets.chomp.to_i
 
-# empty container for the Santa collection
+# set empty container for the Santa collection
 santas = []
 gender = ["agender","female","bigender","male", "female","gender fluid", "N/A"]
 ethnicity = ["black","Latino","white","Japanese","prefer not to say","Mystical Creature(unicorn)","N/A"]
 
-gender.length.times do |time|
-  santas << Santa.new(gender[time],ethnicity[time])
+# randomly select values from gender array
+  # randomly generate index
+  # get length of array
+  gender_array_length = gender.length - 1
+  ethinic_array_length = ethnicity.length - 1
+  random_santas = []
+for santa_amount in 1..slave_amount
+
+  ethinic_number = rand(ethinic_array_length)
+  gen_number = rand(gender_array_length)
+
+  ran_gender = gender[gen_number]
+  ran_ethnicity = ethnicity[ethinic_number]
+
+# randomly select values from ethnicity array
+# have the random generated array plug into class argument
+  random_santas << Santa.new(ran_gender, ran_ethnicity)
 end
 
-gender2 = ["cat","dog","fat"]
-ethnicity2 = ["crazy","wild","cookie"]
 
-# combine each of gender2 with ethnicity2 one by one
-gender2.length.times do |t|
-  santas << Santa.new(gender2[t], ethnicity2[t])
+
+random_santas.length.times do |t|
+  puts ""
+  puts random_santas[t].user_gender
+  puts random_santas[t].age
+  puts random_santas[t].user_ethnicity
 end
-
-x = Santa.new("boy","shit")
-x.celebrate_birthday
