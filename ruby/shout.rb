0000308da7d3f66a8b8e2f2a3a5eb3d91eeaecf3
RGHuynh <randall.g.huynh@gmail.com>
2017-10-27 11:58:42 -0400

    Add mixin version of Shout module
    The shout module will be called by the class outside of the module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#
+#   def self.yelling_happily
+#     puts "crazy!!"
+#   end
+# end
+#
+# p Shout.yell_angrily("hey")
+# p Shout.yelling_happily
+
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    p words + "!!!" + " :("
   end
 
-  def self.yelling_happily
+  def yelling_happily
     puts "crazy!!"
   end
 end
 
-p Shout.yell_angrily("hey")
-p Shout.yelling_happily
+class Person
+  include Shout
+end
+
+class CrazyPerson
+  include Shout
+end
+
+tommy = Person.new
+tommy.yell_angrily("crazy")
+
+
+boby = CrazyPerson.new
+boby.yelling_happily
