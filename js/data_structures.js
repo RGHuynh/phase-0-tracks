a78462535e03328f23416b3b057eb49f13192327
RGHuynh <randall.g.huynh@gmail.com>
2017-11-01 15:35:22 -0400

    Add horse objects to file
    The horse objects uses the horseName array as its key and color as its
    values.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,13 @@
 var color = ["black", "green","brown","white"]
-var hourseName = ["Tom","James", "Bob", "Kid"]
+var horseName = ["Tom","James", "Bob", "Kid"]
 
-color.push("purple")
-hourseName.push("Boy")
-console.log(color, hourseName)
+color.push("purple");
+horseName.push("Boy");
+console.log(color, horseName);
+
+var horse = {};
+for(var i = 0; i <= (color.length - 1); i++){
+  horse[horseName[i]] = color[i];
+}
+
+console.log(horse)
