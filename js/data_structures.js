14a40a4426b5682fd54c3a69dbd871f55336ecf6
RGHuynh <randall.g.huynh@gmail.com>
2017-11-03 03:35:26 -0400

    Add Car construtor function
    The Car construtor will take in the color and the name. Then it will
    print out the color and the name when carData method is called.

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,3 +11,17 @@ for(var i = 0; i <= (color.length - 1); i++){
 }
 
 console.log(horse)
+
+
+function Car(color, name){
+  this.color = color;
+  this.name = name;
+
+  this.carData = function(){
+    console.log("The Car color is " + color);
+    console.log("The Name of your car is " + name);
+  };
+}
+
+var car1 = new Car("blue", "Tom")
+car1.carData()
