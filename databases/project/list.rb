ff18fcc560da075ccdc4c7f431c5d5eef3910b73
RGHuynh <randall.g.huynh@gmail.com>
2017-11-11 00:48:08 -0500

    Add user input for title

--- databases/project/list.rb
+++ databases/project/list.rb
@@ -1,6 +1,10 @@
+require "sqlite3"
+
 # ADD CURRENT DATE
 current_date = Time.now.strftime("%d/%m/%Y")
 
 # ADD USER FOR TITLE
+p "Add title"
+title = gets.chomp
 # ASK USER FOR THINGS THEY WANT TO ADD
 #   IF THERE IS MORE, KEEP ASKING
